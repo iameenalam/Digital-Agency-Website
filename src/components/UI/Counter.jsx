a39c957ce46c6104056463531eaf80630c7c4090
Ameen Alam~ <ameenalam98@gmail.com>
2024-07-16 15:51:11 +0500

    created Counter.jsx and counter.css file and also made a few changes in the overall styling

---
+++ src/components/UI/Counter.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../../styles/counter.css";
+
+const counterData = [
+    {
+        number: 100,
+        text: 'Clients'
+    },
+    {
+        number: 6,
+        text: 'Running Projects'
+    },
+    {
+        number: 60,
+        text: 'Projects Completed'
+    },
+]
+
+const Counter = () => {
+  return (
+    <section className="counter" id="projects">
+      <div className="container">
+        <div className="counter__wrapper">
+        {
+            counterData.map((item,index)=>(
+                <div className="counter__item" key={index}>
+                    <h3 className="counter__number">{item.number}+</h3>
+                    <h4 className="counter__title">{item.text}</h4>
+                </div>
+            ))
+        }
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
