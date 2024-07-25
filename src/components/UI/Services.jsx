e39fc8b1f689ebe1eb3f12b3cefa9bd462143ce9
Ameen Alam~ <ameenalam98@gmail.com>
2024-07-25 07:21:08 +0500

    created about us section

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -3,23 +3,23 @@ import '../../styles/services.css'
 
 const serviceData = [
     {
+        icon: 'ri-apps-line',
+        title: 'App Development',
+        desc: 'Lorem ipsum dolor sit amet elit. Illo repellat intore impedit autem quae fugiat pront facilis que!'
+    },
+    {
         icon: 'ri-global-line',
         title: 'Web Development',
         desc: 'Lorem ipsum dolor sit amet elit. Illo repellat intore impedit autem quae fugiat pront facilis que!'
     },
     {
-        icon: 'ri-server-line',
-        title: 'Web Hosting',
+        icon: 'ri-code-s-slash-line',
+        title: 'Web Design',
         desc: 'Lorem ipsum dolor sit amet elit. Illo repellat intore impedit autem quae fugiat pront facilis que!'
     },
     {
         icon: 'ri-line-chart-line',
         title: 'Data Analytics',
-        desc: 'Lorem ipsum dolor sit amet elit. Illo repellat intore impedit autem quae fugiat pront facilis que!'
-    },
-    {
-        icon: 'ri-robot-2-line',
-        title: 'Generative AI',
         desc: 'Lorem ipsum dolor sit amet elit. Illo repellat intore impedit autem quae fugiat pront facilis que!'
     },
 ]
@@ -29,8 +29,9 @@ const Services = () => {
     <section id="services">
         <div className="container">
             <div className="services__top-content">
-                <h2>Elevate Your Business With</h2>
-                <h2 className='highlight'> Our Best Services</h2>
+                <h6 className='subtitle'>Our Services</h6>
+                <h2>Elevate your business with</h2>
+                <h2 className='highlight'> our best services</h2>
             </div>
 
             <div className="service__item-wrapper">
