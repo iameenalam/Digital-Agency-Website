e7d5b000b668ae4b188bd8f6f37141005ee53bcd
Ameen Alam~ <ameenalam98@gmail.com>
2024-07-30 03:27:14 +0500

    completed projects section

--- src/components/UI/Projects.jsx
+++ src/components/UI/Projects.jsx
@@ -1,33 +1,100 @@
-import React from 'react'
-import '../../styles/projects.css'
-import Project1 from '../../images/keeper-app.PNG'
+import React from "react";
+import Slider from "react-slick";
+import "../../styles/projects.css";
+import Project1 from "../../images/keeper-app.PNG";
+import Project2 from "../../images/tin-dog.PNG";
 
 const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
     <section>
-        <div class="container">
+      <div className="container">
         <div className="project__top-content">
           <h6 className="subtitle">Our Projects</h6>
           <h2>
-            Let's have a look from our
+            Let's have a look at our
             <span className="highlight"> recent projects</span>
           </h2>
         </div>
-      <div class="all-projects">
-        <div class="project-item">
-          <div class="project-info">
-            <h1>Project 1</h1>
-            <h2>Keeper App</h2>
-            <p>Crafted with React, Keeper App is a web-based note-taking application designed to simplify the process of creating and deleting notes. With its user-friendly interface, the app ensures a seamless and organized experience, inspired by Google Keep.</p>
+        <Slider {...settings} className="all-projects">
+          <div className="project-item">
+            <div className="project-info">
+              <h1>Project 1</h1>
+              <h2>Keeper-App</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessbus cupppiditate modi consequuntur explicabo magnam!
+              </p>
+              <div className="project-icons">
+                <span>
+                  <a
+                    href="https://ameenz-keeper-app.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="ri-earth-line"></i>
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://github.com/iameenalam/Keeper-App"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="ri-github-fill"></i>
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="project-img">
+              <img src={Project1} alt="Keeper App" />
+            </div>
           </div>
-          <div class="project-img">
-            <img src={Project1} alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
-    </section>
-  )
-}
 
-export default Projects
+          <div className="project-item">
+            <div className="project-info">
+              <h1>Project 2</h1>
+              <h2>TinDog</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessbus cupppiditate modi consequuntur explicabo magnam!
+              </p>
+              <div className="project-icons">
+                <span>
+                  <a
+                    href="https://tin-dog-startup-website.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="ri-earth-line"></i>
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://github.com/iameenalam/TinDog-Startup-Website"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="ri-github-fill"></i>
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="project-img">
+              <img src={Project2} alt="TinDog" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
